deff71174b32f588632044d51a9d04ff0d72a958
roozbeh sharifnasab <rsharifnasab@gmail.com>
2020-02-25 00:09:06 +0330

    cleaned multiplaction table

--- MultplicationTable.pas
+++ MultplicationTable.pas
@@ -1,10 +1,12 @@
-program zarb;
-var i,j : integer;
-	begin
-	for i:=1 to 10 do begin
-	for j:=1 to 10 do
-	write (i*j:5);
-	writeln;
+program MultiplactionTable;
+var
+	i,j : integer;
+begin
+	for i:=1 to 10 do
+ 	begin
+		for j:=1 to 10 do
+			write (i*j:5);
+		writeln;
 	end;
 	readln
 end.
